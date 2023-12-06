b3a41f13b4f7a95a38a4b50c80c8d65ffd93eabb
Justine Cacho <justinecacho@gmail.com>
2023-12-06 12:34:49 -0800

    adds week 7 changes

---
+++ Week 7/fetchFromMongoAPI2.mjs
@@ -0,0 +1,47 @@
+// fetchFromMongoAPI2.mjs
+
+const fetchDocumentFromMongoDB = async () => {
+    const api_endpoint = "https://us-west-2.aws.data.mongodb-api.com/app/data-muvrr/endpoint/data/v1/action/findOne";
+  
+    const myBody = {
+      "collection": "listingsAndReviews",
+      "database": "sample_airbnb",
+      "dataSource": "Cluster0",
+      "filter": {
+        "beds": 2,
+        "property_type": "House"
+      },
+      "projection": {
+        "_id": 1,
+        "beds": 1,
+        "name": 1,
+        "bedrooms": 1
+      }
+    };
+  
+    console.log("Fetching document from:", {
+      dataSource: myBody.dataSource,
+      database: myBody.database,
+      collection: myBody.collection
+    });
+  
+    try {
+      const response = await fetch(api_endpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'api-key': 'WasXvNOgXLQ3yocnHqZBMD5kdP6cFEqiNDdBu8klMveeECphpRFtKK5EacrNSEBF',
+        },
+        body: JSON.stringify(myBody)
+      });
+  
+      const json_response = await response.json();
+      console.log(json_response);
+    } catch (error) {
+      console.error("Error fetching document:", error.message);
+    }
+  };
+  
+  // Call the function to initiate the fetch
+  fetchDocumentFromMongoDB();
+  
